da2ac90a6e06188e0106a792fc96079942b01480
Hexagon Six <hexagon6@fet.li>
2016-09-07 11:31:34 +0200

    Initial commit (Page 28)

---
+++ src/pages/Detail.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Chance from 'chance';
+
+class Detail extends React.Component {
+  buttonClicked() {
+    this.forceUpdate();
+  }
+  
+  render() {
+    return (<div>
+    <p>Hello, {chance.first()}.</p>
+    <p>You are from {chance.country({ full: true })}.</p>
+    <button onClick={this.buttonClicked.bind(this)}>Meet Someone New</button>
+    </div>);
+  }
+}
+export default Detail;
